4c852f7939922e4af3774caec07d1ae85dfa5dc6
Martin-Nordeback <113906826+Martin-Nordeback@users.noreply.github.com>
2025-04-05 12:20:07 +0200

    remove piss

--- SharedUtilsPackage/Package.swift
+++ SharedUtilsPackage/Package.swift
@@ -13,9 +13,6 @@ let package = Package(
             targets: ["SharedUtilsPackage"]
         ),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Martin-Nordeback/SharedUtils", branch: "main")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
